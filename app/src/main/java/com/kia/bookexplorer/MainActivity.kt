29ed7cfeb76d32fdc5edfa96109816d2f1f3a378
hamidreza <hr.shadabkia@gmail.com>
2024-09-21 09:52:08 +0330

    first commit

---
+++ app/src/main/java/com/kia/bookexplorer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kia.bookexplorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
